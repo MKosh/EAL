0bcc1e231489ba001c61ca466fdef62a055544ba
Mark Mekosh <markm@MM-Surface-Laptop.localdomain>
2022-06-07 20:23:17 -0400

    Minor reorganizing

--- app/EALapplication.cc
+++ app/EALapplication.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ranges>
+#include <vector>
 #include "TDOMParser.h"
 #include "TXMLDocument.h"
 #include "TXMLNode.h"
@@ -6,13 +8,15 @@
 #include "../include/EAL/Samples.h"
 #include "../include/EAL/XMLInterface.h"
 
+
 int main() {
   EAL::test();
-  auto node = EAL::XML::Initialize("datasets/dataset_2016.xml");
+  auto node = EAL::XML::Initialize("../datasets/dataset_2016.xml");
 
   node = node->GetChildren()->GetNextNode();
   
   //TList* attr_list = node.get()->GetAttributes();
   std::cout << node->GetNodeName() << "\n";
+
   return 0;
 }
