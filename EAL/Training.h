e84ba0419a588d15b2d8cd7dfc5069963025ed0a
Mark Mekosh <markmekosh@gmail.com>
2022-09-17 13:15:38 -0400

    Getting training to work

--- EAL/Training.h
+++ EAL/Training.h
@@ -44,6 +44,7 @@ public:
   std::vector<std::string> m_training_variables;
   std::vector<std::string> m_training_spectators;
   std::vector<std::string> m_all_variables; // training variables and spectators
+  std::unordered_map<std::string, std::string> m_variable_types;
 
   TMVATraining(std::string settings_file) {
     std::ifstream TMVA_settings(settings_file);
@@ -60,11 +61,13 @@ public:
       for (const auto& variable : settings["variables"]) {
         m_training_variables.emplace_back(variable.at("name"));
         m_all_variables.emplace_back(variable.at("name"));
+        m_variable_types.insert(std::make_pair(variable.at("name"),variable.at("type")));
       }
 
       for (const auto& spectator : settings["spectators"]) {
         m_training_spectators.emplace_back(spectator.at("name"));
         m_all_variables.emplace_back(spectator.at("name"));
+        m_variable_types.insert(std::make_pair(spectator.at("name"),spectator.at("type")));
       }
 
     }
--- app/EALapplication.cc
+++ app/EALapplication.cc
@@ -6,10 +6,16 @@
 #include <map>
 #include <fstream>
 #include <functional>
+#include <string_view>
 #include "TFile.h"
 #include "TObjArray.h"  
 #include "TCanvas.h"
 #include "TTree.h"
+#include "TCut.h"
+#include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/Tools.h"
+#include "TMVA/TMVAGui.h"
 #include "ROOT/RDataFrame.hxx"
 #include "../include/nlohmann/json.hpp"
 #include "../EAL/Analysis.h"
@@ -18,11 +24,13 @@
 /** @endcond */
 using json = nlohmann::json;
 
-void ProcessesDataFrame(std::vector<std::string>& file_names, std::string& tree_name,
+void ProcessesDataFrame(std::vector<std::string>& file_names, std::string_view tree_name,
                               EAL::ML::TMVATraining& train,
                               EAL::Cut::DefinesList& defines,
                               EAL::Cut::CutList& cut_list,
                               std::function<bool(int32_t)> class_cut,
+                              std::string_view out_tree_name,
+                              std::string_view out_file_name,
                               ROOT::RDF::RSnapshotOptions& opts) {
   ROOT::RDataFrame df(tree_name, file_names, train.m_all_variables);
   df.DefinePerSample("process_id", defines.set_process_ids)
@@ -48,7 +56,82 @@ void ProcessesDataFrame(std::vector<std::string>& file_names, std::string& tree_
     .Filter(cut_list.zepp_had_cut, {"zeppHad", "vbf_deta"}, "zepp_had")
     .Filter(cut_list.wv_sr_cut, {"bos_PuppiAK8_m_sd0"}, "wv_sr")
     .Filter(class_cut, {"class_id"})
-    .Snapshot("data", "intermediate3.root", train.m_all_variables, opts);
+    .Snapshot(out_tree_name, out_file_name, train.m_all_variables, opts);
+}
+
+void CreateIntermediateRootFiles(EAL::Cut::DefinesList& defines,
+                                  EAL::Cut::CutList& df_cuts,
+                                  EAL::Cut::DataOnly& data_cut,
+                                  EAL::Cut::SignalOnly& signal_cut,
+                                  EAL::Cut::BackgroundOnly& background_cut,
+                                  std::vector<std::string>& data_files,
+                                  std::vector<std::string>& signal_files,
+                                  std::vector<std::string>& background_files,
+                                  EAL::ML::TMVATraining& train,
+                                  std::string_view out_file) {
+  std::cout << "EAL : Creating intermediate ROOT Files\n\n    :";
+  ROOT::RDF::RSnapshotOptions opts;
+  opts.fMode = "RECREATE";
+
+  std::cout << "    : filtering Data dataframe\n    :\n";
+  ProcessesDataFrame(data_files, "Events", train, defines, df_cuts, data_cut, "data", out_file, opts);
+
+  opts.fMode = "UPDATE";
+  std::cout << "    : filtering Signal dataframe\n    :\n";
+  ProcessesDataFrame(signal_files, "Events", train, defines, df_cuts, signal_cut, "signal", out_file, opts);
+
+  std::cout << "    : filtering Background dataframe\n    :\n";
+  ProcessesDataFrame(background_files, "Events", train, defines, df_cuts, background_cut, "background", out_file, opts);
+
+  std::cout << "EAL : Done writing intermediate ROOT Files\n\n";
+}
+
+std::unique_ptr<TMVA::DataLoader> LoadData(EAL::ML::TMVATraining train,
+                                            std::string in_file) {
+  auto input_file = TFile::Open(in_file.c_str());
+  auto signal_tree = input_file->Get<TTree>("signal");
+  auto background_tree = input_file->Get<TTree>("background");
+
+  auto data_loader = std::make_unique<TMVA::DataLoader>("dataset");
+  data_loader->AddSignalTree(signal_tree);
+  data_loader->SetSignalWeightExpression("mcWeight*genWeight");
+  data_loader->AddBackgroundTree(background_tree);
+  data_loader->SetBackgroundWeightExpression("mcWeight*genWeight");
+
+  for (const auto& variable : train.m_training_variables) {
+    data_loader->AddVariable(variable);
+  }
+  for (const auto& spectator : train.m_training_spectators) {
+    data_loader->AddSpectator(spectator, train.m_variable_types.at(spectator)[0]);
+  }
+
+  TCut signal_cut = "";
+  TCut background_cut = "";
+  data_loader->PrepareTrainingAndTestTree(signal_cut, background_cut, 
+    "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V");
+
+  return data_loader;
+}
+
+std::unique_ptr<TMVA::Factory> CreateTMVAFactory(TMVA::DataLoader* data_loader, 
+                                                  EAL::ML::TMVATraining train,
+                                                  TFile* out_file) {
+  auto factory = std::make_unique<TMVA::Factory>("TMVAClassification", out_file, 
+                                                  "!V:!Silent:Color:DrawProgressBar:"
+                                                  "Transformations=I;D;P;G,D:"
+                                                  "AnalysisType=Classification");
+
+  for (const auto& method : train.m_methods) {
+    if (method.m_type == "kBDT") {
+      factory->BookMethod(data_loader, TMVA::Types::kBDT, method.m_name, method.m_options);
+    } else if (method.m_type == "kDL") {
+      factory->BookMethod(data_loader, TMVA::Types::kDL, method.m_name, method.m_options);
+    } else {
+      std::cout << "EAL ERROR : I Don't recognize TMVA method - " << method.m_type << '\n';
+    }
+  }
+
+  return factory;
 }
 
 int EALapplication() {
@@ -83,168 +166,26 @@ int EALapplication() {
   bool debugging = false;
 
   EAL::ML::TMVATraining train("data/config/TMVA_settings.json");
-  std::cout << "\nAnalysis beginning:\n--- Creating intermediate ROOT files\n\n";
-/*
-  auto SetProcessID = [&process_IDs](unsigned int slot, const ROOT::RDF::RSampleInfo& id){
-    int32_t pid = -999;
-    for (const auto& process : process_IDs) {
-      if (id.Contains(process.first)) { pid = process.second; }
-    }
-    return pid;
-  };
+  std::cout << "\nEAL : Analysis beginning\n    : \n";
 
-  std::string current_dataframe = "data";
-  auto SetSampleYear = [&current_dataframe, debugging](unsigned int slot, const ROOT::RDF::RSampleInfo& id){
-    if (debugging) std::cout << "Processing " << current_dataframe << " dataframe\n--- " << id.AsString() << "\n\n";
-    if (id.Contains("2016")) { return 2016; }
-    if (id.Contains("2017")) { return 2017; }
-    if (id.Contains("2018")) { return 2018; }
-    return 0;
-  };
-
-  auto SetSampleLumin = [&anl, class_IDs](unsigned int slot, const ROOT::RDF::RSampleInfo& id){
-    for (const auto& smpl : anl.GetAnalysisSamples()) {
-      if (id.Contains(smpl.m_file_name) && class_IDs.at(smpl.m_file_name) 
-        != static_cast<int32_t>(EAL::ClassID::kData)) { 
-        return smpl.m_luminosity;
-      }
-      else if (id.Contains(smpl.m_file_name) && class_IDs.at(smpl.m_file_name) 
-        == static_cast<int32_t>(EAL::ClassID::kData)) { 
-        return 1.0f;
-      }
-    }
-    return -999.0f;
-  };
-
-  auto SetSampleWeight = [&anl](unsigned int slot, const ROOT::RDF::RSampleInfo& id){
-    for (const auto& smpl : anl.GetAnalysisSamples()) {
-      if (id.Contains(smpl.m_file_name)) {
-        //std::cout << "sample: " << smpl.m_sample << "\nWeight: " << smpl.m_sample_weight <<'\n';
-        return smpl.m_sample_weight; 
-      }
-    }
-    return -999.0f;
-  };
-
-  auto SetSampleClass = [&class_IDs](unsigned int slot, const ROOT::RDF::RSampleInfo& id) {
-    for (const auto& classes : class_IDs) {
-      if (id.Contains(classes.first)) { return classes.second; }
-    }
-    return -999;
-  };
-*/
-  //EAL::Cut::Tau21 tau21_cut;
-  //EAL::Cut::QGid qgid_cut;
-  //EAL::Cut::WVBoosted wv_boosted_cut;
-  //EAL::Cut::LepPt lep_pt_cut;
-  //EAL::Cut::LepEta lep_eta_cut;
-  //EAL::Cut::FatjetPt fatjet_pt_cut;
-  //EAL::Cut::FatjetEta fatjet_eta_cut;
-  //EAL::Cut::FatjetTau21 fatjet_tau21_cut;
-  //EAL::Cut::VBSJetsMjj vbs_jets_mjj_cut;
-  //EAL::Cut::VBSJetsPt vbs_jets_pt_cut;
-  //EAL::Cut::VBSDeltaEta vbs_deta_cut;
-  //EAL::Cut::METpt met_pt_cut;
-  //EAL::Cut::bTagVeto btag_veto_cut;
-  //EAL::Cut::Isolation iso_cut;
-  //EAL::Cut::ZeppLep zepp_lep_cut;
-  //EAL::Cut::ZeppHad zepp_had_cut;
-  //EAL::Cut::WVSignalRegion wv_sr_cut;
   EAL::Cut::DefinesList defines{process_IDs, class_IDs, samples};
   EAL::Cut::CutList df_cuts;
   EAL::Cut::DataOnly data_cut;
   EAL::Cut::SignalOnly signal_cut;
   EAL::Cut::BackgroundOnly background_cut;
+  std::string input_file = "intermediate6.root";
+  std::string output_file = "TMVAoutput.root";
+  std::unique_ptr<TFile> out_file(TFile::Open(output_file.c_str(), "RECREATE"));
+  //CreateIntermediateRootFiles(defines, df_cuts, data_cut, signal_cut, background_cut, data_files, signal_files, background_files, train, output_file);
 
-  ROOT::RDF::RSnapshotOptions opts;
-  opts.fMode = "RECREATE";
+  auto data_loader = LoadData(train, input_file);
+  auto factory = CreateTMVAFactory(data_loader.get(), train, out_file.get());
 
-  std::cout << "    filtering Data dataframe\n\n";
-  ROOT::RDataFrame df("Events", data_files, train.m_all_variables);
-  auto modified_df = df.DefinePerSample("process_id", defines.set_process_ids)
-                        .DefinePerSample("lumi", defines.set_sample_lumi)
-                        .DefinePerSample("year", defines.set_sample_year)
-                        .DefinePerSample("mcWeight", defines.set_sample_weight)
-                        .DefinePerSample("class_id", defines.set_sample_class)
-                        .Filter(df_cuts.tau21_cut, {"bos_PuppiAK8_tau2tau1"}, "tau21_cut")
-                        .Filter(df_cuts.qgid_cut, {"vbf1_AK4_qgid","vbf2_AK4_qgid"}, "qgid_cut")
-                        .Filter(df_cuts.wv_boosted_cut, {"lep2_pt", "bos_PuppiAK8_pt"}, "wv_boosted")
-                        .Filter(df_cuts.lep_pt_cut, {"lep1_pt"}, "lep_pt")
-                        .Filter(df_cuts.lep_eta_cut, {"lep1_m", "lep1_eta"}, "lep_eta")
-                        .Filter(df_cuts.fatjet_pt_cut, {"bos_PuppiAK8_pt"}, "fatjet_pt")
-                        .Filter(df_cuts.fatjet_eta_cut, {"bos_PuppiAK8_eta"}, "fatjet_eta")
-                        .Filter(df_cuts.fatjet_tau21_cut, {"bos_PuppiAK8_tau2tau1"}, "fatjet_tau21")
-                        .Filter(df_cuts.vbs_jets_mjj_cut, {"vbf_m"}, "vbs_jets_mjj")
-                        .Filter(df_cuts.vbs_jets_pt_cut, {"vbf1_AK4_pt", "vbf2_AK4_pt"}, "vbs_jets_pt")
-                        .Filter(df_cuts.vbs_deta_cut, {"vbf_deta"}, "vbs_deta")
-                        .Filter(df_cuts.met_pt_cut, {"MET"}, "met_pt")
-                        .Filter(df_cuts.btag_veto_cut, {"nBtag_loose"}, "btag_veto")
-                        .Filter(df_cuts.iso_cut, {"AntiIsoInt", "bosCent"}, "isolation")
-                        .Filter(df_cuts.zepp_lep_cut, {"zeppLep", "vbf_deta"}, "zepp_lep")
-                        .Filter(df_cuts.zepp_had_cut, {"zeppHad", "vbf_deta"}, "zepp_had")
-                        .Filter(df_cuts.wv_sr_cut, {"bos_PuppiAK8_m_sd0"}, "wv_sr")
-                        .Filter(data_cut, {"class_id"})
-                        .Snapshot("data", "intermediate5.root", train.m_all_variables, opts);
+  factory->TrainAllMethods();
+  factory->TestAllMethods();
+  factory->EvaluateAllMethods();
 
-  opts.fMode = "UPDATE";
-  current_dataframe = "Signal"; 
-  std::cout << "    filtering Signal dataframe\n\n";
-
-  ROOT::RDataFrame df2("Events", signal_files, train.m_all_variables);
-  auto signal_df = df2.DefinePerSample("process_id", SetProcessID)
-                        .DefinePerSample("lumi", SetSampleLumin)
-                        .DefinePerSample("year", SetSampleYear)
-                        .DefinePerSample("mcWeight", SetSampleWeight)
-                        .DefinePerSample("class_id", SetSampleClass)
-                        .Filter(tau21_cut, {"bos_PuppiAK8_tau2tau1"}, "tau21_cut")
-                        .Filter(qgid_cut, {"vbf1_AK4_qgid","vbf2_AK4_qgid"}, "qgid_cut")
-                        .Filter(wv_boosted_cut, {"lep2_pt", "bos_PuppiAK8_pt"}, "wv_boosted")
-                        .Filter(lep_pt_cut, {"lep1_pt"}, "lep_pt")
-                        .Filter(lep_eta_cut, {"lep1_m", "lep1_eta"}, "lep_eta")
-                        .Filter(fatjet_pt_cut, {"bos_PuppiAK8_pt"}, "fatjet_pt")
-                        .Filter(fatjet_eta_cut, {"bos_PuppiAK8_eta"}, "fatjet_eta")
-                        .Filter(fatjet_tau21_cut, {"bos_PuppiAK8_tau2tau1"}, "fatjet_tau21")
-                        .Filter(vbs_jets_mjj_cut, {"vbf_m"}, "vbs_jets_mjj")
-                        .Filter(vbs_jets_pt_cut, {"vbf1_AK4_pt", "vbf2_AK4_pt"}, "vbs_jets_pt")
-                        .Filter(vbs_deta_cut, {"vbf_deta"}, "vbs_deta")
-                        .Filter(met_pt_cut, {"MET"}, "met_pt")
-                        .Filter(btag_veto_cut, {"nBtag_loose"}, "btag_veto")
-                        .Filter(iso_cut, {"AntiIsoInt", "bosCent"}, "isolation")
-                        .Filter(zepp_lep_cut, {"zeppLep", "vbf_deta"}, "zepp_lep")
-                        .Filter(zepp_had_cut, {"zeppHad", "vbf_deta"}, "zepp_had")
-                        .Filter(wv_sr_cut, {"bos_PuppiAK8_m_sd0"}, "wv_sr")
-                        .Filter(signal_cut, {"class_id"})
-                        .Snapshot("signal", "intermediate5.root", train.m_all_variables, opts);
-
-  current_dataframe = "background";
-  std::cout << "    filtering Background dataframe\n";
-  ROOT::RDataFrame df3("Events", background_files, train.m_all_variables);
-  auto background_df = df3.DefinePerSample("process_id", SetProcessID)
-                        .DefinePerSample("lumi", SetSampleLumin)
-                        .DefinePerSample("year", SetSampleYear)
-                        .DefinePerSample("mcWeight", SetSampleWeight)
-                        .DefinePerSample("class_id", SetSampleClass)
-                        .Filter(tau21_cut, {"bos_PuppiAK8_tau2tau1"}, "tau21_cut")
-                        .Filter(qgid_cut, {"vbf1_AK4_qgid","vbf2_AK4_qgid"}, "qgid_cut")
-                        .Filter(wv_boosted_cut, {"lep2_pt", "bos_PuppiAK8_pt"}, "wv_boosted")
-                        .Filter(lep_pt_cut, {"lep1_pt"}, "lep_pt")
-                        .Filter(lep_eta_cut, {"lep1_m", "lep1_eta"}, "lep_eta")
-                        .Filter(fatjet_pt_cut, {"bos_PuppiAK8_pt"}, "fatjet_pt")
-                        .Filter(fatjet_eta_cut, {"bos_PuppiAK8_eta"}, "fatjet_eta")
-                        .Filter(fatjet_tau21_cut, {"bos_PuppiAK8_tau2tau1"}, "fatjet_tau21")
-                        .Filter(vbs_jets_mjj_cut, {"vbf_m"}, "vbs_jets_mjj")
-                        .Filter(vbs_jets_pt_cut, {"vbf1_AK4_pt", "vbf2_AK4_pt"}, "vbs_jets_pt")
-                        .Filter(vbs_deta_cut, {"vbf_deta"}, "vbs_deta")
-                        .Filter(met_pt_cut, {"MET"}, "met_pt")
-                        .Filter(btag_veto_cut, {"nBtag_loose"}, "btag_veto")
-                        .Filter(iso_cut, {"AntiIsoInt", "bosCent"}, "isolation")
-                        .Filter(zepp_lep_cut, {"zeppLep", "vbf_deta"}, "zepp_lep")
-                        .Filter(zepp_had_cut, {"zeppHad", "vbf_deta"}, "zepp_had")
-                        .Filter(wv_sr_cut, {"bos_PuppiAK8_m_sd0"}, "wv_sr")
-                        .Filter(background_cut, {"class_id"})
-                        .Snapshot("background", "intermediate5.root", train.m_all_variables, opts);
-
-  std::cout << "\n--- Done!\n";
+  std::cout << "\nEAL : Done!\n";
   return 0;
 }
 
